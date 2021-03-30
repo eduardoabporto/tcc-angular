fe024af2243cafd58e708df5d25b9fe0ebc185d5
eduardoabporto <eduardo.porto@innovasys.com.br>
2021-03-30 08:50:57 -0300

    Publicação Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURLBase: 'https://tcc-eduardoabporto-spring.herokuapp.com',
+  clientId: 'my-angular-app',
+  clientSecret: '@321',
+  obterTokenUrl: '/oauth/token'
 };
